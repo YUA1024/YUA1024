4cb89ac476c6344ab421cc2cafa61a2a503596b7
YUA1024 <59056069+YUA1024@users.noreply.github.com>
2021-05-27 10:49:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,19 @@
-####
+import streamlit as st
+import numpy as np
+import pandas as pd
+#import kornia
+from torch import nn
+import torch
+#from torchvision.transforms import functional as F
+#from torchvision.utils import make_grid
+#from streamlit_ace import st_ace
+from PIL import Image
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    im = Image.open(uploaded_file)
+else:
+    im = Image.open(r"F:\Applied Computer Version\clouds\1111.jfif")
+scaler = int(im.height / 2)
+st.sidebar.image(im, caption="Input Image", width=256)
+#image = F.pil_to_tensor(im).float() / 255
