f156718692c4a38da924683964af4de7f6bdc1c0
doublefishmmm <84852752+doublefishmmm@users.noreply.github.com>
2021-05-28 16:22:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,17 @@ import pandas as pd
 #from streamlit_ace import st_ace
 from PIL import Image
 
-st.title('Cloud detection')
-st.write("You can display the image in full size by hovering it and clicking the double arrow")
-st.write(":point_left:Please select **Choose the image you want to detect** in the sidebar to start.")
+st.title('Cloud detection :cloud:')
+'''
+Our cloud detection application can identify the type of cloud you upload and display it
+
+:point_left:Please select **Choose the image you want to detect** in the sidebar to start
+
+You can display the image in full size by hovering it and clicking the double arrow
+'''
+
 # sidebar
-uploaded_file = st.sidebar.file_uploader("Choose a file")
+uploaded_file = st.sidebar.file_uploader("Choose a image you want to detect")
 if uploaded_file is not None:
     im = Image.open(uploaded_file)
 else:
@@ -21,5 +27,5 @@ else:
 st.sidebar.image(im, caption="Input Image", width=256)
 st.sidebar.write("")
 
-st.image(im, caption='the cloud you choose', use_column_width=True)
+st.image(im, caption='the cloud you choose', width=1024)
 
