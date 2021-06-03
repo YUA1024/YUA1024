1c12fe9062e7cddbb9c303cd70c6a3c8a40908c0
doublefishmmm <84852752+doublefishmmm@users.noreply.github.com>
2021-06-03 23:45:26 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
 import os
+
+# Style Images Data
+
+content_images_file = ['Pedestrians on the highway1.jpeg', 'test2.jpeg']
+
+content_images_name = ['Pedestrians on the highway', 'biker and trafficlight']
+
+images_path = 'image'
+
+content_images_dict = {name: os.path.join(images_path, filee) for name, filee in zip(content_images_name, content_images_file)}
+
